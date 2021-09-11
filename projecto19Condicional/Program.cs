a42dde53e04b202fef325352fdb64340a41e2a19
william <factzure@hotmail.com>
2021-09-10 21:24:24 -0500

    Agregar archivos de proyecto.

---
+++ projecto19Condicional/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecto19Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sueldo;
+            Console.WriteLine("Ingrese su sueldo ? ");
+            sueldo = float.Parse(Console.ReadLine());
+
+            if (sueldo >3000)
+            {
+                Console.WriteLine("Debe abonar impuestos");
+            }
+            else
+            {
+                Console.WriteLine("No debe abonar impuestos");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
